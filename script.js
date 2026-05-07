43efbc4046706c0188f227963886ac86cdcf56c0
derly-g <derly394@gmail.com>
2026-05-07 12:32:37 -0500

    Implement project card carousel navigation
    Add carousel functionality for project cards with next and previous navigation.

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x'); 
   navbar.classList.toggle('active');
 }   
+
+const projectBox = document.querySelector('.project-box');
+const cards = document.querySelectorAll('.project-card');
+
+let currentIndex = 0;
+
+function updateCarousel() {
+  const cardWidth = cards[0].offsetWidth + 48;
+
+  projectBox.scrollTo({
+    left: currentIndex * cardWidth,
+    behavior: 'smooth'
+  });
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+  if (currentIndex < cards.length - 1) {
+    currentIndex++;
+  } else {
+    currentIndex = 0;
+  }
+
+  updateCarousel();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+  } else {
+    currentIndex = cards.length - 1;
+  }
+
+  updateCarousel();
+});

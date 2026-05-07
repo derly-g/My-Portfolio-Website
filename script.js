b4ce8de869d684744a397a4707038f809ecdbe7d
derly-g <derly394@gmail.com>
2026-05-06 19:21:37 -0500

    Fix typo in ClassList to classList

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-  menuIcon.ClassList.toggle('bx-x');
+  menuIcon.ClassList.toggle('bx-x')
   navbar.classList.toggle('active');
 }

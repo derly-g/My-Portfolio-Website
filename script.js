adc0d23d9ed463642765b617049f3cbcb97c2e14
derly-g <derly394@gmail.com>
2026-05-06 19:19:26 -0500

    Fix classList typo in menu toggle function

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-  menuIcon.ClassList.toggle('bx-x')
-  navbar.ClassList.toggle('active');
+  menuIcon.ClassList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }

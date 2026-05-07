a38ceeeec058f90d01c6297cd25c85dbe081f755
derly-g <derly394@gmail.com>
2026-05-06 19:09:59 -0500

    Add menu toggle functionality to script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  menuIcon.ClassList.toggle('bx-x')
+  navbar.ClassList.toggle('active');
+}

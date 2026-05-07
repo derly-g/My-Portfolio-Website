921c9a97accdec4c43aea9467c619ea402b7b03a
derly-g <derly394@gmail.com>
2026-05-07 13:45:51 -0500

    Refine comments for menu icon and carousel logic
    Clarified comments in script.js regarding menu icon functionality and carousel transition calculations.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle('bx-x'); 
   navbar.classList.toggle('active');
 }   
-///I adjusted the navbar to be a little menu icon and for it to be able to close and open//
+///I adjusted the navbar to be a little menu icon and for it to be able to close and open its already in the css so when it is toggled it makes it into an x so you can close it//
 const projectBox = document.querySelector('.project-box');
 const cards = document.querySelectorAll('.project-card');
 
@@ -13,7 +13,7 @@ let currentIndex = 0;
 
 function updateCarousel() {
   const cardWidth = cards[0].offsetWidth + 48;
-
+///0 card is the first one so this calculates the cards so that they're able to transition it takes the 300px of the card and translates it to + 48 for js when moving to the next one//
   projectBox.scrollTo({
     left: currentIndex * cardWidth,
     behavior: 'smooth'
